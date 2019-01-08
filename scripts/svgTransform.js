57279241438784817713df8e5b45c334b74437a8
Aaron Stadler <Aaron.Stadler@LighthouseSystems.com>
2019-01-08 15:26:45 -0500

    finish triangles, adjust bio with mobile

--- scripts/svgTransform.js
+++ scripts/svgTransform.js
@@ -3,8 +3,12 @@
 (function(){
     var initialTheta = 0;
     var currentTheta = initialTheta;
-    var thetaDelta= 0.5;
+    var thetaDelta= 10;
     var angularLimit = 180;
+    var moveLimit = 60;
+    var moveStep = 10;
+    var currentMovement = initialTheta
+    var flipped = false;
 
     var animationFrameID;
     var tri1 = document.getElementById("tri1");
@@ -15,25 +19,70 @@
     var transformObj=triangles.createSVGTransform();
     tris.forEach(function(t){
         t.transform.baseVal.appendItem(transformObj);
+        t.transform.baseVal.appendItem(transformObj);
     })
 
-    document.getElementById("triangleGraphic").addEventListener("mouseover", function(event){
-        animationFrameID = requestAnimationFrame(flipTriangle);
+    
+    window.addEventListener("scroll", function(event){
+        if(!flipped && $(document).scrollTop() > 1200){
+            animationFrameID = requestAnimationFrame(flipTriangle);
+            flipped = true;
+        }
+        
+        //animationMoveFrameID = requestAnimationFrame(translateTriangle);
     });
 
     function flipTriangle(){
         if(currentTheta <= angularLimit){
-            tris.forEach(function(t){
-                t.transform.baseVal.getItem(0).setRotate(currentTheta, t.getAttribute("x"), t.getAttribute("y"));
-                currentTheta += thetaDelta;
-                animationFrameID = requestAnimationFrame(flipTriangle);
-            });
+            if(currentMovement <= moveLimit){
+                tri1.transform.baseVal.getItem(1).setTranslate(0, currentMovement);
+                tri2.transform.baseVal.getItem(1).setTranslate(0, -currentMovement);
+                tri3.transform.baseVal.getItem(1).setTranslate(0, -currentMovement);
+            }
+            tri1.transform.baseVal.getItem(0).setRotate(currentTheta,400, 250 + currentMovement);
+            tri2.transform.baseVal.getItem(0).setRotate(currentTheta,200, 550 - currentMovement);
+            tri3.transform.baseVal.getItem(0).setRotate(currentTheta,600, 550 - currentMovement);
+            
+            currentTheta += thetaDelta;
+            currentMovement += moveStep;
+            animationFrameID = requestAnimationFrame(flipTriangle);
         }
-        else{
+        else{ 
+            //currentMovement = 0;
             cancelAnimationFrame(animationFrameID);
             return;
         }
     }
+    /*
+    This got out of hand, will finish later
+
+    function inverseFlipTriangle(){
+        if(currentTheta <= angularLimit){
+            if(currentMovement >= 0){
+                tri1.transform.baseVal.getItem(1).setTranslate(0, -currentMovement);
+                tri2.transform.baseVal.getItem(1).setTranslate(0, currentMovement);
+                tri3.transform.baseVal.getItem(1).setTranslate(0, currentMovement);
+            }
+            tri1.transform.baseVal.getItem(0).setRotate(currentTheta,400, 250 + currentMovement);
+            tri2.transform.baseVal.getItem(0).setRotate(currentTheta,200, 550 - currentMovement);
+            tri3.transform.baseVal.getItem(0).setRotate(currentTheta,600, 550 - currentMovement);
+            
+            currentTheta += thetaDelta;
+            currentMovement -= moveStep;
+            animationFrameID = requestAnimationFrame(inverseFlipTriangle);
+        }
+        else{
+            SET ANGLE BACK TO 0 FROM 360 
+            tri1.transform.baseVal.getItem(0).setRotate(initialTheta,400, 250 - currentMovement);
+            tri2.transform.baseVal.getItem(0).setRotate(initialTheta,200, 550 + currentMovement);
+            tri3.transform.baseVal.getItem(0).setRotate(initialTheta,600, 550 + currentMovement); 
+            currentTheta = initialTheta;
+            angularLimit = 180;
+            cancelAnimationFrame(animationFrameID);
+            return;
+        }
+    }
+    */
 }()
     
 );
